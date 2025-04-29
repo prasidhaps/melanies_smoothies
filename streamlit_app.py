dabfceccefa44c7ec6ca18b4c0f520dc5e9f808c
prasidhaps <prasidha.ps@gmail.com>
2025-04-28 23:53:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-
+cnx = st.connection("snowflake")
+session = cns.session()
 
 
 # Write directly to the app
